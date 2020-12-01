427f165b8d30baa5b079ed31c752763a0fa6ae92
teddyxiong53 <1073167306@qq.com>
2020-12-01 13:46:47 +0800

    注册界面可以显示出来

--- mysite/user/urls.py
+++ mysite/user/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('register', views.RegisterView.as_view(), name='register'),
-
+    path('logout', views.RegisterView.as_view(), name='logout'),
+    path('login', views.RegisterView.as_view(), name='login'),
+    # path('logout', views.RegisterView.as_view(), name='logout'),
 ]
